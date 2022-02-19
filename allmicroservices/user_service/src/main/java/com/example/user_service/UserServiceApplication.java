644a0d9fd557a98e92743eded1cac9546659b63b
Nikunj-bisht <nikkubisht12@gmail.com>
2022-02-19 13:13:05 +0530

    hhf

--- allmicroservices/user_service/src/main/java/com/example/user_service/UserServiceApplication.java
+++ allmicroservices/user_service/src/main/java/com/example/user_service/UserServiceApplication.java
@@ -9,6 +9,7 @@ public class UserServiceApplication {
 	public static void main(String[] args) {
 		System.out.println("food palaxaxa 123 i");
 		System.out.println("food");
+		System.out.println("prac");
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
