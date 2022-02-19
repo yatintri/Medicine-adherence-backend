644a1168522037c0e386dc56268db551e79c2ada
vinay <vinay.kumar@nineleaps.com>
2022-02-19 12:02:38 +0530

    changed by vinay

--- allmicroservices/user_service/src/main/java/com/example/user_service/UserServiceApplication.java
+++ allmicroservices/user_service/src/main/java/com/example/user_service/UserServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
-		System.out.println("f");
+		System.out.println("food palaxaxa ");
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
