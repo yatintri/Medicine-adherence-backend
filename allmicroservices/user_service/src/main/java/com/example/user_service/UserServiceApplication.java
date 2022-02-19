d0aa0d5ae5bdd0e7f7735431ccf5837afe8691a2
Nikunj-bisht <nikkubisht12@gmail.com>
2022-02-19 17:08:49 +0530

    imported dependencies

--- allmicroservices/user_service/src/main/java/com/example/user_service/UserServiceApplication.java
+++ allmicroservices/user_service/src/main/java/com/example/user_service/UserServiceApplication.java
@@ -7,9 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
-		System.out.println("food palaxaxa 123 i");
-		System.out.println("food");
-		System.out.println("prac");
+
+
+
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
