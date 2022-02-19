32e01e36afaaad256bdf646e47bd26e2d5dbaa92
Nikunj-bisht <nikkubisht12@gmail.com>
2022-02-19 17:40:11 +0530

    made user entity

---
+++ allmicroservices/user_service/src/main/java/com/example/user_service/model/UserEntity.java
@@ -0,0 +1,28 @@
+package com.example.user_service.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "user")
+public class UserEntity {
+
+  @Id
+  @Column(name = "user_id")
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int user_id;
+
+  @Column(name = "user_name")
+    private String user_name;
+
+  @Column(name = "email")
+    private String email;
+
+
+}
