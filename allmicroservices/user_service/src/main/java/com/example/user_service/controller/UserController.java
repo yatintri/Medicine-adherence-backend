cc35da531a7b9c5d6344268837861f38b9f1d4df
Nikunj-bisht <nikkubisht12@gmail.com>
2022-02-19 17:37:04 +0530

    made user entity

--- allmicroservices/user_service/src/main/java/com/example/user_service/controller/UserController.java
+++ allmicroservices/user_service/src/main/java/com/example/user_service/controller/UserController.java
@@ -7,8 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/uesrs")
 public class UserController {
+
     @GetMapping("/1")
     public void show() {
         System.out.println("Showing uesrs.");
     }
+
 }
