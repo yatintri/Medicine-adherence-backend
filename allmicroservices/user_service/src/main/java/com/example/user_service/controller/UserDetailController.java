0b60d3f001c12754ab74b1908cead0144a5293ed
vinay <vinay.kumar@nineleaps.com>
2022-02-20 15:17:24 +0530

    fixing

---
+++ allmicroservices/user_service/src/main/java/com/example/user_service/controller/UserDetailController.java
@@ -0,0 +1,28 @@
+package com.example.user_service.controller;
+
+
+
+import com.example.user_service.model.UserDetails;
+
+import com.example.user_service.service.UserDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/userdetails")
+public class UserDetailController {
+
+
+//     Vinay is pro
+    @Autowired
+    private UserDetailService userDetailService;
+    @PostMapping(value = "/saveuserdetails/{id}" , produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> saveUserDetails(@PathVariable("id") int id, @RequestBody UserDetails userDetails) {
+         return new ResponseEntity<>(userDetailService.saveUserDetail(id,userDetails),HttpStatus.CREATED);
+
+    }
+
+}
