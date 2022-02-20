69305eeaa8c0115b978643894746d659959c94b3
Nikunj-bisht <nikkubisht12@gmail.com>
2022-02-20 10:56:34 +0530

    some fixes

---
+++ allmicroservices/user_service/src/main/java/com/example/user_service/config/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package com.example.user_service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(getinfo());
+    }
+
+    public ApiInfo getinfo(){
+
+        return new ApiInfo(
+                "Medicine adherence user_service REST API",
+                "Some custom description of API.",
+                "API TOS",
+                "Terms of service",
+                new Contact("Nineleaps", "www.example.com", "myeaddress@company.com"),
+                "License of API", "API license URL", Collections.emptyList());
+
+    }
+
+
+
+}
