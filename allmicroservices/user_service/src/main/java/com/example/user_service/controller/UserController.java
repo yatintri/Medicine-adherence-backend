d88b2ec5d06c157e5da6744c408d404d9a85ca30
Nikunj-bisht <nikkubisht12@gmail.com>
2022-02-20 15:27:56 +0530

    updated

--- allmicroservices/user_service/src/main/java/com/example/user_service/controller/UserController.java
+++ allmicroservices/user_service/src/main/java/com/example/user_service/controller/UserController.java
@@ -11,7 +11,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -21,7 +20,7 @@ public class UserController {
 
     @Autowired
     private UserService userService;
-
+    //updated
 
     @PostMapping(value = "/saveuser" , produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> saveUser(@RequestBody UserEntity userEntity) throws UserexceptionMessage {
