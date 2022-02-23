bbffd18bb856fc9da579784da2762a40cb02a629
yatintri <yatintripathi0679@gmail.com>
2022-02-23 17:26:59 +0530

    Changed the fetchtype

--- allmicroservices/user_service/src/main/java/com/example/user_service/repository/UserCaretakerRepository.java
+++ allmicroservices/user_service/src/main/java/com/example/user_service/repository/UserCaretakerRepository.java
@@ -2,6 +2,9 @@ package com.example.user_service.repository;
 
 import com.example.user_service.model.UserCaretaker;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface UserCaretakerRepository extends JpaRepository<UserCaretaker, String> {
 
