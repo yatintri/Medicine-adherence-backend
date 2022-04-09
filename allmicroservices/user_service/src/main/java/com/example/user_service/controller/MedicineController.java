033c4f63bf4636d4e95750d49f6a8cea595cf86c
Nikunj-bisht <nikkubisht12@gmail.com>
2022-04-09 21:41:24 +0530

    refactored send image

--- allmicroservices/user_service/src/main/java/com/example/user_service/controller/MedicineController.java
+++ allmicroservices/user_service/src/main/java/com/example/user_service/controller/MedicineController.java
@@ -1,6 +1,7 @@
 package com.example.user_service.controller;
 
-
+import com.example.user_service.exception.UserMedicineException;
+import com.example.user_service.exception.UserexceptionMessage;
 import com.example.user_service.model.*;
 import com.example.user_service.pojos.Medicinepojo;
 import com.example.user_service.repository.UserMedicineRepository;
@@ -14,6 +15,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(path = "/api/usermedicine")
@@ -28,32 +31,70 @@ public class MedicineController {
     @Autowired
     UserMedicineRepository userMedicineRepository;
 
+    // save caretaker for a patients
+    @PostMapping(value = "/savemedicine/{id}" , produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> saveUserMedicine(@PathVariable("userId") String id, @RequestBody UserMedicines userMedicines)throws UserMedicineException , UserexceptionMessage {
+
+        return new ResponseEntity<>(userMedicineService.saveUserMedicine(id,userMedicines), HttpStatus.CREATED);
+
+    }
+
+    @PutMapping(value = "/updatestatus/{id}" , produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> updateMedicineStatus(@PathVariable("medId") Integer medId)throws UserMedicineException{
+
+        return new ResponseEntity<>(userMedicineService.updateMedicineStatus(medId),HttpStatus.CREATED);
+
+    }
+
+    @GetMapping(value = "/getusermedicine/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getUserMedicines(@PathVariable("userId") String userId) throws UserMedicineException,UserexceptionMessage,ExecutionException, InterruptedException {
+
+
+        return new ResponseEntity<>(userMedicineService.getallUserMedicines(userId).get(), HttpStatus.OK);
+
+    }
+
+    @PutMapping(value = "/editmedicine/{id}" , produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> editMedicineDetails(@PathVariable("medId") Integer id,
+                                               @RequestBody UserMedicines userMedicines) throws UserMedicineException,UserexceptionMessage {
+
+        return new ResponseEntity<>(userMedicineService.editMedicineDetails(id,userMedicines),HttpStatus.CREATED);
+
+    }
+
     @PostMapping(value = "/syncmedicines",produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> syncdata(@RequestParam("userId") String userId , @RequestBody List<Medicinepojo> medicinepojo){
+    public ResponseEntity<?> syncdata(@RequestParam("userId") String userId , @RequestBody List<Medicinepojo> medicinepojo){
 
-        UserEntity userEntity = userRepository.getuserbyid(userId);
-        List<UserMedicines> userMedicinesList = new ArrayList<>();
-        for(Medicinepojo medicinepojo1 : medicinepojo) {
+       UserEntity userEntity = userRepository.getuserbyid(userId);
 
-            UserMedicines userMedicines = new UserMedicines();
+        List<UserMedicines> userMedicinesList = medicinepojo.stream().map(medicinepojo1 -> {
+                    UserMedicines userMedicines = new UserMedicines();
 
-            userMedicines.setMedicineDes(medicinepojo1.getMedicineDes());
-            userMedicines.setMedicineName(medicinepojo1.getMedicineName());
-            userMedicines.setDays(medicinepojo1.getDays());
-            userMedicines.setMedicineId(medicinepojo1.getUserId());
-            userMedicines.setEndDate(medicinepojo1.getEndDate());
-            userMedicines.setTitle(medicinepojo1.getTitle());
-            userMedicines.setCurrentCount(medicinepojo1.getCurrentCount());
-            userMedicines.setTotalMedReminders(medicinepojo1.getTotalMedReminders());
-            userMedicines.setStartDate(medicinepojo1.getStartDate());
-            userMedicines.setTime(medicinepojo1.getTime());
-            userMedicines.setUserEntity(userEntity);
-            userMedicinesList.add(userMedicines);
-        }
+                    userMedicines.setMedicineDes(medicinepojo1.getMedicineDes());
+                    userMedicines.setMedicineName(medicinepojo1.getMedicineName());
+                    userMedicines.setDays(medicinepojo1.getDays());
+                    userMedicines.setMedicineId(medicinepojo1.getUserId());
+                    userMedicines.setEndDate(medicinepojo1.getEndDate());
+                    userMedicines.setTitle(medicinepojo1.getTitle());
+                    userMedicines.setCurrentCount(medicinepojo1.getCurrentCount());
+                    userMedicines.setTotalMedReminders(medicinepojo1.getTotalMedReminders());
+                    userMedicines.setStartDate(medicinepojo1.getStartDate());
+                    userMedicines.setTime(medicinepojo1.getTime());
+                    userMedicines.setUserEntity(userEntity);
 
-        userMedicineRepository.saveAll(userMedicinesList);
+                    return userMedicines;
+                            })
+               .collect(Collectors.toList());
 
-        return new ResponseEntity<>("OK",HttpStatus.OK);
+
+
+       userMedicineRepository.saveAll(userMedicinesList);
+
+
+        // userMedicineService.syncdata(user_id,userMedicines);
+
+
+       return new ResponseEntity<>("",HttpStatus.OK);
 
 
     }
