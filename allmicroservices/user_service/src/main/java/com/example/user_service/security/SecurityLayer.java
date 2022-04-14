088d8be2fc1657856e868994a9ff372a9d39b5f0
Nikunj-bisht <nikkubisht12@gmail.com>
2022-04-14 22:31:12 +0530

    jwt update

--- allmicroservices/user_service/src/main/java/com/example/user_service/security/SecurityLayer.java
+++ allmicroservices/user_service/src/main/java/com/example/user_service/security/SecurityLayer.java
@@ -2,35 +2,23 @@ package com.example.user_service.security;
 
 import com.example.user_service.config.filter.UserDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
-@Order(1)
+
 public class SecurityLayer extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private UserDetailService userDetailService;
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailService);
-    }
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
     }
 
 
